ca6b68595f05c51812ef934025fbd993a7a316f7
Dan Johnson <dan@danj.eu>
2016-12-19 23:51:35 +0000

    Added Get Started instructions

---
+++ resources/views/getting-started.php
@@ -0,0 +1,60 @@
+<div class="padding-top">
+
+	<div class="container">
+		<div class="row">
+			<div class="column">
+				<h1>Getting Started</h1>
+				<p>
+					Before you jump right in, you'll need two things:
+					<ul>
+						<li>Node</li>
+						<li>A MongoDB Database</li>
+					</ul>
+
+					<small>
+						<strong>Haven't got the resources to run a database?</strong> You can point Zappem to an MongoDB database. You should check out <a href="https://mlab.com">mLab</a> - they offer free MongoDB databases.
+					</small>
+
+				</p>
+
+				<h1>Downloading Zappem</h1>
+
+				<p>
+					The fastest way to download Zappem is via NPM
+				</p>
+<pre>
+$ npm install zappem -g
+</pre>
+				<p>
+					Once Zappem is installed, you can run it using the following command
+				</p>
+<pre>
+$ zappem start
+</pre>
+				
+				<h1>Configuring Zappem</h1>
+				<p>
+					On first run of Zappem, you'll be presented with the configuration helper. This will guide you through the steps to connect Zappem to your database, and also allow you to configure custom options such as what port number Zappem should run on.
+				</p>
+
+				<h1>Connecting your application to Zappem</h1>
+				<p>
+					Now you need to tell your application to send all errors to Zappem. Lucky for you, we've already built some connectors for a range of different platforms. Just find the one that suits your application and read the README for further instructions.
+				</p>
+				<ul>
+					<li>
+						<a href="https://github.com/danjohnson95/zappem-laravel">Laravel</a>
+					</li>
+				</ul>
+
+				<strong>That's all there is to it!</strong>
+				<p>
+					Stuck? Check the docs for further information, or if you're really stuck, feel free to drop me an email. <a href="mailto:dan@danj.eu">dan@danj.eu</a>
+				</p>
+
+
+			</div>
+		</div>
+	</div>
+
+</div>
