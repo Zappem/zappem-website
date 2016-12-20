6fe62ec264272a3f075c96880ff4cecc3816baff
Dan Johnson <dan@danj.eu>
2016-12-20 00:06:11 +0000

    Added menu for docs

--- resources/views/docs.php
+++ resources/views/docs.php
@@ -1,3 +1,49 @@
-<div class="white-section">
+<div class="padding-top">
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="column docs-menu">
+				<nav>
+					<h2>Menu</h2>
+					<ul>
+						<li>Overview</li>
+						<li>CLI
+							<ul>
+								<li>Configuration</li>
+								<li>Stats</li>
+							</ul>
+						</li>
+						<li>API
+							<ul>
+								<li><label>POST</label> /exceptions</li>
+								<li><label>GET</label> /projects</li>
+								<li><label>GET</label> /:projectid/team</li>
+								<li><label>GET</label> /:projectid/exceptions</li>
+								<li><label>GET</label> /:projectid/exception/:exceptionid</li>
+							</ul>
+						</li>
+						<li>Interface
+							<ul>
+								<li>Dashboard</li>
+								<li>Exceptions Table</li>
+								<li>Exception</li>
+								<li>Users</li>
+								<li>Teams</li>
+								<li>Uptime</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+
+			<div class="column">
+				Welcome to the docs. wip
+			</div>
+
+		</div>
+
+	</div>
 
 </div>
